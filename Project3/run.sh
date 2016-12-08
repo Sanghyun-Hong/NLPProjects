64fe2696510b3cb9aa6e7b7fca0c993cb2472cbb
Octavian Suciu <osuciu@tdcuckoo02.proxy.umiacs.umd.edu>
2016-12-08 13:10:33 -0500

    added the baseline prediction for one training instance

--- Project3/run.sh
+++ Project3/run.sh
@@ -5,11 +5,14 @@
 #DATASET=plagiarism # Pearson: 0.82125
 #DATASET=postediting # Pearson: 0.83391
 #DATASET=question-question # Pearson: 0.65056
+DATASET=MSRvid
 
 echo "Dataset: $DATASET"
 
-IN_FILE=datasets+scoring_script/test/STS2016.input.$DATASET.txt
-OUT_FILE=output/out.$DATASET.txt
+IN_FILE=datasets+scoring_script/train/STS2012-en-test/STS.input.$DATASET.txt
+OUT_FILE=datasets+scoring_script/output_analysis/out.$DATASET.txt
+#IN_FILE=datasets+scoring_script/test/STS2016.input.$DATASET.txt
+#OUT_FILE=output/out.$DATASET.txt
 
 python predict_baseline.py $IN_FILE $OUT_FILE
 python get_correlation.py $IN_FILE $OUT_FILE
